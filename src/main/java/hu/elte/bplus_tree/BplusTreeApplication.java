5678b73d19dd1296cb06d9a8db2c200a2fb2b25e
Novák Lilla <novaklilla0621@gmail.com>
2021-12-22 15:50:57 +0100

    uploading whole project

---
+++ src/main/java/hu/elte/bplus_tree/BplusTreeApplication.java
@@ -0,0 +1,14 @@
+package hu.elte.bplus_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BplusTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BplusTreeApplication.class, args);
+	}
+
+}
